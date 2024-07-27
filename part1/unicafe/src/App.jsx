db7780d9c87073c5164fdc3c594a1780effc5bb2
Yilin Wang <sa22039@student.samk.fi>
2024-07-28 00:12:10 +0300

    1.11*: unicafe step 6

--- part1/unicafe/src/App.jsx
+++ part1/unicafe/src/App.jsx
@@ -1,97 +1,66 @@
-import { useState } from 'react'
-const Statistics = (props) => {
-  if (props.allClicks === 0) {
+import React, { useState } from 'react';
+
+const StatisticLine = (props) => {
+  return (<tr><td>{props.text}</td><td>{props.value}</td></tr>);
+}
+
+const Statistics = ({ good, neutral, bad, allClicks }) => {
+  if (allClicks === 0) {
     return (
       <div>
         No feedback given
       </div>
-    )
+    );
   }
+
+  const positive = (good * 100) / allClicks;
+  const average = (good - bad) / allClicks;
+
   return (
-    <div>
-      <p>good {props.good}</p>
-      <p>neutral {props.neutral}</p>
-      <p>bad {props.bad}</p>
-      <p>all {props.good+ props.neutral+ props.bad}</p>
-      <p>average {(props.good-props.bad)/(props.good+ props.neutral+ props.bad)}</p>
-      <p>average {(props.good)*100/(props.good+ props.neutral+ props.bad)} %</p>
-    </div>
-  )
+    <table>
+      <tbody>
+      <StatisticLine text="good" value={good} />
+      <StatisticLine text="neutral" value={neutral} />
+      <StatisticLine text="bad" value={bad} />
+      <StatisticLine text="all" value={allClicks} />
+      <StatisticLine text="average" value={average} />
+      <StatisticLine text="positive" value={`${positive} %`} />
+      </tbody>
+    </table>
+  );
 }
 
-
 const App = () => {
-  const [good, setGood] = useState(0)
-  const [neutral, setNeutral] = useState(0)
-  const [bad, setBad] = useState(0)
-
+  const [good, setGood] = useState(0);
+  const [neutral, setNeutral] = useState(0);
+  const [bad, setBad] = useState(0);
 
   const handleGoodClick = () => {
-    setGood(good + 1)
+    setGood(good + 1);
   }
+  
   const handleNeutralClick = () => {
-    setNeutral(neutral+1)
+    setNeutral(neutral + 1);
   }
 
   const handleBadClick = () => {
-    setBad(bad + 1)
+    setBad(bad + 1);
   }
 
-  console.log(good,neutral, bad)
+  const allClicks = good + neutral + bad;
+
   return (
     <div>
-      <h1>give feedback</h1>
+      <h1>Give Feedback</h1>
       
-      <button onClick={handleGoodClick}>good</button>
-      <button onClick={handleNeutralClick}>neutral</button>
-      <button onClick={handleBadClick}>bad</button>
-      <h1>statistics</h1>
-      <Statistics good={good} neutral={neutral} bad={bad} allClicks = {good+neutral+bad}/>
+      <button onClick={handleGoodClick}>Good</button>
+      <button onClick={handleNeutralClick}>Neutral</button>
+      <button onClick={handleBadClick}>Bad</button>
+      
+      <h1>Statistics</h1>
+      <Statistics good={good} neutral={neutral} bad={bad} allClicks={allClicks} />
     </div>
-  )
+  );
 }
 
-// const Display = (props) => {
-//   return (
-//     <div>{props.counter}</div>
-//   )
-// }
-
-// const Button = (props) => {
-//   return (
-//     <button onClick={props.onClick}>
-//       {props.text}
-//     </button>
-//   )
-// }
-
-// const App = () => {
-//   const [counter, setCounter] = useState(0)
-//   console.log('rendering with counter value', counter)
-
-//   const increaseByOne = () => {
-//     console.log('increasing, value before', counter)
-//     setCounter(counter + 1)
-//   }
-
-//   const decreaseByOne = () => { 
-//     console.log('decreasing, value before', counter)
-//     setCounter(counter - 1)
-//   }
-
-//   const setToZero = () => {
-//     console.log('resetting to zero, value before', counter)
-//     setCounter(0)
-//   }
-
-//   return (
-//     <div>
-//       <Display counter={counter} />
-//       <Button onClick={increaseByOne} text="plus" />
-//       <Button onClick={setToZero} text="zero" />
-//       <Button onClick={decreaseByOne} text="minus" />
-//     </div>
-//   )
-// } 
-
-export default App
+export default App;

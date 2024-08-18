d4ccc7b20583f93235e5b8a7dbbb039d3701a0e8
Yilin Wang <sa22039@student.samk.fi>
2024-08-18 19:44:28 +0300

    2.8: The Phonebook Step 3

--- part2/phonebook/src/App.jsx
+++ part2/phonebook/src/App.jsx
@@ -4,25 +4,31 @@ import Note from './components/Note'
 const App = (props) => {
   const [persons, setPersons] = useState(props.persons)
   const [newName, setNewName] = useState('') 
+  const [newNumber, setNewNumber] = useState('') 
   // const [showAll, setShowAll] = useState(true)
 
   const addPhonebook = (event) => {
     event.preventDefault()
     const noteObject = {
       name: newName,
-      number: '+000 9008080',
+      number: newNumber,
       id: String(persons.length + 1),
     }
     console.log('button clicked', event.target)
 
     setPersons(persons.concat(noteObject))
     setNewName('')
+    setNewNumber('')
     alert(newName+' is already added to phonebook!');
   }
 
-  const handleNoteChange = (event) => {
+  const handleNameChange = (event) => {
     console.log(event.target.value)
     setNewName(event.target.value)
+  }
+  const handleNumberChange = (event) => {
+    console.log(event.target.value)
+    setNewNumber(event.target.value)
   }
 
   // const notesToShow = showAll
@@ -43,7 +49,14 @@ const App = (props) => {
           <p style={{ display: 'inline'}}>name: </p>
           <input
             value={newName}
-            onChange={handleNoteChange}
+            onChange={handleNameChange}
+          />
+        </div>
+        <div>
+          <p style={{ display: 'inline'}}>number: </p>
+          <input
+            value={newNumber}
+            onChange={handleNumberChange}
           />
         </div>
         <button type="submit">add</button>
